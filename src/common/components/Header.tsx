d56aa26f6411f73154b6c979d8406622f2e1c93e
nacht <nacht42@riseup.net>
2021-04-19 23:39:57 +0800

    feat(header): add button to toggle theme in header

--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -1,7 +1,47 @@
-import { IconButton } from "@chakra-ui/button";
-import { Container, Flex, Heading } from "@chakra-ui/layout";
-import { IoLogoGithub } from "react-icons/io5";
+import { Container, Flex, Heading, HStack, LinkProps } from "@chakra-ui/layout";
 import React from "react";
+import {
+  Link,
+  Icon,
+  IconButton,
+  IconButtonProps,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { IoLogoGithub, IoMoon, IoSunny } from "react-icons/io5";
+
+type GithubLinkProps = {
+  href: string;
+} & Omit<LinkProps, "aria-label" | "onClick" | "icon">;
+
+const GithubLink: React.FC<GithubLinkProps> = (props) => {
+  const { href } = props;
+  return (
+    <Link aria-label="github link" href={href}>
+      <Icon w={6} h={6} as={IoLogoGithub} />
+    </Link>
+  );
+};
+
+type ThemeToggleButtonProps = Omit<
+  IconButtonProps,
+  "aria-label" | "onClick" | "icon"
+>;
+
+export const ThemeToggleButton: React.FC<ThemeToggleButtonProps> = (props) => {
+  const { toggleColorMode } = useColorMode();
+  const SwitchIcon = useColorModeValue(IoMoon, IoSunny);
+  return (
+    <IconButton
+      aria-label="toggle theme"
+      onClick={toggleColorMode}
+      variant="ghost"
+      fontSize="2xl"
+      icon={<Icon as={SwitchIcon} />}
+      {...props}
+    />
+  );
+};
 
 export const Header: React.FC = () => {
   return (
@@ -9,12 +49,10 @@ export const Header: React.FC = () => {
       <Flex justify="start" align="center">
         <Heading size="lg">PDF Editer</Heading>
         <Flex justify="end" flex="1">
-          <IconButton
-            aria-label="github link"
-            variant="link"
-            fontSize="2xl"
-            icon={<IoLogoGithub />}
-          />
+          <HStack spacing={2}>
+            <GithubLink href="https://github.com/nacht42/offline-pdf-editor" />
+            <ThemeToggleButton />
+          </HStack>
         </Flex>
       </Flex>
     </Container>
