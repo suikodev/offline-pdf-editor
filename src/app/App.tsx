8bc8df6f67219d950dfb01367b691f2b39ffeb62
nacht <nacht42@riseup.net>
2021-04-26 21:03:18 +0800

    refactor(*): to reuse Header, move Header to App root

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import routes from "../constants/routes.json";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Home } from "../pages/home";
+import { Header } from "../common/components/Header";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path={routes.HOME}>
-          <Home />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <>
+      <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route path={routes.HOME}>
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { Header } from "../../common/components/Header";
 import { HomeBanner } from "./components/HomeBanner";
 
 export const Home: React.FC = () => {
   return (
     <>
-      <Header />
       <HomeBanner />
     </>
   );
