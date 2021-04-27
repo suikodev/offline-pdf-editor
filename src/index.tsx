f3a9d5014809ac600197e6fe9539fddd64b5ce30
nacht <nacht42@riseup.net>
2021-04-27 21:22:53 +0800

    feat(*): add custom typed hook for redux api

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/App";
-import store from "./app/store";
+import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
