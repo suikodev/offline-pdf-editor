0ef9d44fa5bc65741bdc98ba180b1b248b0410c5
nacht <nacht42@riseup.net>
2021-04-29 20:52:38 +0800

    refactor(*): promote callback when succeed to store pdf file

--- src/common/components/ChoosePdfButton.tsx
+++ src/common/components/ChoosePdfButton.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { SelectFileButton, SelectFileButtonProps } from "./SelectFileButton";
 import { nanoid } from "@reduxjs/toolkit";
-import { useHistory } from "react-router";
-import router from "../../constants/routes.json";
 import { PdfStore } from "../storage";
 import { Pdf } from "../storage/PdfStore";
 
@@ -11,20 +9,23 @@ import { Pdf } from "../storage/PdfStore";
  */
 export const ChoosePdfButton: React.FC<ChoosePdfButtonProps> = (props) => {
   const [isLoading, setIsLoading] = useState(false);
-  const history = useHistory();
 
   const handlePdfSelect = async (files: FileList) => {
-    const PdfUint8Array = new Uint8Array(await files[0].arrayBuffer());
-    setIsLoading(true);
-    const pdfId = nanoid();
-    const pdf: Pdf = {
-      id: pdfId,
-      filename: files[0].name,
-      content: PdfUint8Array,
-    };
-    await PdfStore.setItem(pdfId, pdf);
-    setIsLoading(false);
-    history.push(router.OVERVIEW);
+    try {
+      const PdfUint8Array = new Uint8Array(await files[0].arrayBuffer());
+      setIsLoading(true);
+      const pdfId = nanoid();
+      const pdf: Pdf = {
+        id: pdfId,
+        filename: files[0].name,
+        content: PdfUint8Array,
+      };
+      await PdfStore.setItem(pdfId, pdf);
+      setIsLoading(false);
+      props.onPdfStoreSuccess?.();
+    } catch (e) {
+      props.onPdfStoreError?.(e);
+    }
   };
   return (
     <SelectFileButton
@@ -37,15 +38,13 @@ export const ChoosePdfButton: React.FC<ChoosePdfButtonProps> = (props) => {
   );
 };
 
-type ChoosePdfButtonProps =
-  | {
-      /**
-       * callback when PDF is stored on browser successfully.
-       */
-      onPdfStoreSuccess?: () => void;
-      /**
-       * callback when PDF fail to be store on browser.
-       */
-      onPdfStoreError?: () => void;
-    }
-  | Omit<SelectFileButtonProps, "onFilesSelected" | "accept">;
+type ChoosePdfButtonProps = {
+  /**
+   * callback when PDF is stored on browser successfully.
+   */
+  onPdfStoreSuccess?: () => void;
+  /**
+   * callback when PDF fail to be store on browser.
+   */
+  onPdfStoreError?: (error: Error) => void;
+} & Omit<SelectFileButtonProps, "onFilesSelected" | "accept">;
--- src/pages/home/components/HomeBanner.tsx
+++ src/pages/home/components/HomeBanner.tsx
@@ -1,8 +1,14 @@
 import { Container, Flex, Heading, Text } from "@chakra-ui/react";
 import React from "react";
+import { useHistory } from "react-router";
+import routes from "../../../constants/routes.json";
 import { ChoosePdfButton } from "../../../common/components/ChoosePdfButton";
 
 export const HomeBanner: React.FC = () => {
+  const history = useHistory();
+  const handlePdfFileStoreSuccess = () => {
+    history.replace(routes.OVERVIEW);
+  };
   return (
     <Container as="section" maxWidth="container.lg">
       <Flex direction="column" align="center" paddingY={["5rem", "10rem"]}>
@@ -12,7 +18,12 @@ export const HomeBanner: React.FC = () => {
           all page have loaded. your PDFs and your personal infomation won&#39;t
           send to any server FOREVER.
         </Text>
-        <ChoosePdfButton size="lg" colorScheme="brand" marginY="2rem">
+        <ChoosePdfButton
+          onPdfStoreSuccess={handlePdfFileStoreSuccess}
+          size="lg"
+          colorScheme="brand"
+          marginY="2rem"
+        >
           choose PDF(s)
         </ChoosePdfButton>
       </Flex>
