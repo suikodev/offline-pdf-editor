840f6ca20f0c3f05502bb193e077cdb115a5b0c3
nacht <nacht42@riseup.net>
2021-05-01 15:28:57 +0800

    feat(home): redirect to /overview when pdfInfoList is not null

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router";
+import { useAppSelector } from "../../common/hooks";
 import { HomeBanner } from "./components/HomeBanner";
+import routes from "../../constants/routes.json";
 
 export const Home: React.FC = () => {
+  const history = useHistory();
+  const isPdfListNotNull = useAppSelector(
+    (state) => state.pdfInfo.pdfInfoList.length > 0
+  );
+  useEffect(() => {
+    isPdfListNotNull && history.replace(routes.OVERVIEW);
+  }, [isPdfListNotNull]);
   return (
     <>
       <HomeBanner />
