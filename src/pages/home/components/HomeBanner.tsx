157495aee5675fea290ae62c85972b9a46a347f7
nacht <nacht42@riseup.net>
2021-05-01 14:44:38 +0800

    feat(home): make title in home banner textAlign center

--- src/pages/home/components/HomeBanner.tsx
+++ src/pages/home/components/HomeBanner.tsx
@@ -12,7 +12,9 @@ export const HomeBanner: React.FC = () => {
   return (
     <Container as="section" maxWidth="container.lg">
       <Flex direction="column" align="center" paddingY={["5rem", "10rem"]}>
-        <Heading size="4xl">Edit your PDF all OFFLINE</Heading>
+        <Heading textAlign="center" size="4xl">
+          Edit your PDF all OFFLINE
+        </Heading>
         <Text fontSize="xl" color="gray.600" textAlign="center" marginY="1rem">
           This is a PDF editor which runs in your browser totally offline after
           all page have loaded. your PDFs and your personal infomation won&#39;t
