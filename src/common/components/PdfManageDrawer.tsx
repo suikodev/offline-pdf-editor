6af2e731fa461ffac9fe5df55b2c9dc203e7e07e
nacht <nacht42@riseup.net>
2021-05-03 20:28:40 +0800

    feat(pdfmanagerdrawer): add button to delete pdf in PdfManagerDrawer component

--- src/common/components/PdfManageDrawer.tsx
+++ src/common/components/PdfManageDrawer.tsx
@@ -8,12 +8,59 @@ import {
   Stack,
   List,
   ListItem,
+  Flex,
+  Text,
+  IconButton,
+  HStack,
+  Tooltip,
 } from "@chakra-ui/react";
 import React from "react";
-import { useAppSelector } from "../hooks";
+import { IoTrash } from "react-icons/io5";
+import { RootState } from "../../app/store";
+import { PdfInfo, remove } from "../../features/pdfInfo/pdfInfoSlice";
+import { useAppDispatch, useAppSelector } from "../hooks";
+
+const selectPdfById = (state: RootState, id: string) =>
+  state.pdfInfo.pdfInfoList.find((i) => i.id === id);
+
+const PdfManageListItem: React.FC<{ id: string }> = (props) => {
+  const { filename, id: pdfId } = useAppSelector((state) =>
+    selectPdfById(state, props.id)
+  ) as PdfInfo;
+
+  const dispatch = useAppDispatch();
+  const removePdf = () => dispatch(remove([pdfId]));
+  return (
+    <ListItem>
+      <Flex justifyContent="space-between" align="center">
+        <Text fontSize="xl" isTruncated>
+          {filename}
+        </Text>
+        <HStack>
+          <Tooltip
+            label={`delete ${filename}`}
+            hasArrow
+            fontSize="md"
+            placement="auto"
+          >
+            <IconButton
+              onClick={removePdf}
+              icon={<IoTrash />}
+              aria-label={`delete ${filename}`}
+              colorScheme="red"
+              size="sm"
+            />
+          </Tooltip>
+        </HStack>
+      </Flex>
+    </ListItem>
+  );
+};
 
 export const PdfManageDrawer: React.FC<PdfManageDrawerProps> = (props) => {
-  const pdfInfoList = useAppSelector((state) => state.pdfInfo.pdfInfoList);
+  const pdfIdList = useAppSelector((state) =>
+    state.pdfInfo.pdfInfoList.map((i) => i.id)
+  );
 
   return (
     <Drawer {...props}>
@@ -21,14 +68,15 @@ export const PdfManageDrawer: React.FC<PdfManageDrawerProps> = (props) => {
         <DrawerContent>
           <DrawerCloseButton />
           <DrawerHeader>All PDF</DrawerHeader>
-          <Stack>
-            <List>
-              {pdfInfoList.map((i) => (
-                <ListItem key={i.id}>{i.filename}</ListItem>
-              ))}
-            </List>
-          </Stack>
-          <DrawerBody></DrawerBody>
+          <DrawerBody>
+            <Stack>
+              <List>
+                {pdfIdList.map((i) => (
+                  <PdfManageListItem key={i} id={i} />
+                ))}
+              </List>
+            </Stack>
+          </DrawerBody>
         </DrawerContent>
       </DrawerOverlay>
     </Drawer>
