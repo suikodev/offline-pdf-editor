d80f81fcafb5c793a972fd7ed9c964567cbf9179
nacht <nacht42@riseup.net>
2021-05-03 18:54:31 +0800

    feat(pages/overview): redirect to home page when there are no pdf in store

--- src/pages/overview/Overview.tsx
+++ src/pages/overview/Overview.tsx
@@ -1,14 +1,25 @@
 import { Container } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { shallowEqual } from "react-redux";
+import { useHistory } from "react-router";
 import { useAppSelector } from "../../common/hooks";
 import { PdfDocument } from "./components/PdfDocument";
+import routes from "../../constants/routes.json";
 
 export const Overview: React.FC = () => {
+  const history = useHistory();
+
   const pdfIdList = useAppSelector(
     (state) => state.pdfInfo.pdfInfoList.map((i) => i.id),
     shallowEqual
   );
+
+  useEffect(() => {
+    if (pdfIdList.length <= 0) {
+      history.replace(routes.HOME);
+    }
+  }, [pdfIdList]);
+
   return (
     <Container as="header" maxWidth="container.xl">
       {pdfIdList.map((id) => (
