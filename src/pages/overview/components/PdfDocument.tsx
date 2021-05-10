facb65e168278ba566e3546a574efc2bb3dc1111
nacht <nacht42@riseup.net>
2021-05-10 19:42:20 +0800

    feat(pdfdocument): make PDF page draggable

--- src/pages/overview/components/PdfDocument.tsx
+++ src/pages/overview/components/PdfDocument.tsx
@@ -1,9 +1,70 @@
-import { Flex, Grid, Heading } from "@chakra-ui/react";
-import React, { useEffect, useMemo, useState } from "react";
+import {
+  Box,
+  Flex,
+  Grid,
+  Heading,
+  HTMLChakraProps,
+  Text,
+} from "@chakra-ui/react";
+import { HTMLMotionProps, motion } from "framer-motion";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { PageProps } from "react-pdf";
 import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 import { Pdf, PdfStore } from "../../../common/storage/PdfStore";
 
+type Merge<P, T> = Omit<P, keyof T> & T;
+type MotionBoxProps = Merge<HTMLChakraProps<"div">, HTMLMotionProps<"div">>;
+
+const MotionBox: React.FC<MotionBoxProps> = motion(Box);
+
+type DraggablePageProps = {
+  dragConstraints: React.RefObject<Element>;
+} & PageProps;
+
+const DraggablePage: React.FC<DraggablePageProps> = (props) => {
+  const [isHovering, setIsHovering] = useState(false);
+  const { dragConstraints, ...pageProps } = props;
+  const [renderSize, setRenderSize] = useState({ width: 0, height: 0 });
+  return (
+    <MotionBox
+      height={renderSize.height}
+      width={renderSize.width}
+      style={{ position: "relative", zIndex: isHovering ? 1 : "auto" }}
+      drag
+      dragConstraints={dragConstraints}
+      dragMomentum={false}
+      whileDrag={{ scale: 0.9 }}
+      onHoverStart={() => setIsHovering(true)}
+      onHoverEnd={() => setIsHovering(false)}
+    >
+      {isHovering && (
+        <Text
+          width="full"
+          background="blackAlpha.800"
+          color="white"
+          position="absolute"
+          left="50%"
+          top="50%"
+          transform="translate(-50%, -50%)"
+          zIndex="modal"
+          textAlign="center"
+        >
+          Drag me
+        </Text>
+      )}
+      <Page
+        {...pageProps}
+        onLoadSuccess={(info) => {
+          setRenderSize({ width: info.width, height: info.height });
+        }}
+      />
+    </MotionBox>
+  );
+};
+
 export const PdfDocument: React.FC<{ pdfId: string }> = (props) => {
+  const constantsRef = useRef(null);
+
   const [pdfFile, setPdfFile] = useState<Pdf>();
   useEffect(() => {
     PdfStore.getItem(props.pdfId).then((pdf) => {
@@ -32,9 +93,14 @@ export const PdfDocument: React.FC<{ pdfId: string }> = (props) => {
               {pdfFile.filename}
             </Heading>
           </Flex>
-          <Grid justifyItems="center" templateColumns="repeat(5, 1fr)" gap={1}>
+          <Grid templateColumns="repeat(5, 1fr)" gap={8} ref={constantsRef}>
             {Array.from(new Array(numPages), (_, index) => (
-              <Page key={index} width={200} pageNumber={index + 1} />
+              <DraggablePage
+                width={200}
+                key={index}
+                dragConstraints={constantsRef}
+                pageNumber={index + 1}
+              />
             ))}
           </Grid>
         </Document>
