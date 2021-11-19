3a75990068bf9255e52e570faffc749fad27c74b
Nacht <nacht42@riseup.net>
2021-11-19 16:49:29 +0800

    feat(homebanner): add background color for home banner

--- src/pages/home/components/HomeBanner.tsx
+++ src/pages/home/components/HomeBanner.tsx
@@ -1,4 +1,11 @@
-import { Container, Flex, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import React from "react";
 import { useHistory } from "react-router";
 import routes from "../../../constants/routes.json";
@@ -7,28 +14,31 @@ import { useTranslation } from "react-i18next";
 
 export const HomeBanner: React.FC = () => {
   const { t } = useTranslation();
+  const backgroundColorValue = useColorModeValue("brand.50", "grey.500");
   const history = useHistory();
   const handlePdfFileStoreSuccess = () => {
     history.replace(routes.OVERVIEW);
   };
   return (
-    <Container as="section" maxWidth="container.lg">
-      <Flex direction="column" align="center" paddingY={["5rem", "10rem"]}>
-        <Heading textAlign="center" size="4xl">
-          {t("home.banner.title")}
-        </Heading>
-        <Text fontSize="xl" color="gray.600" textAlign="center" marginY="1rem">
-          {t("home.banner.description")}
-        </Text>
-        <ChoosePdfButton
-          onPdfStoreSuccess={handlePdfFileStoreSuccess}
-          size="lg"
-          colorScheme="blue"
-          marginY="2rem"
-        >
-          {t("home.banner.choosePdfButtonText")}
-        </ChoosePdfButton>
-      </Flex>
-    </Container>
+    <Box as="section" backgroundColor={backgroundColorValue}>
+      <Container maxWidth="container.lg">
+        <Flex direction="column" align="center" paddingY={["5rem", "10rem"]}>
+          <Heading textAlign="center" size="4xl">
+            {t("home.banner.title")}
+          </Heading>
+          <Text fontSize="xl" textAlign="center" marginY="1rem">
+            {t("home.banner.description")}
+          </Text>
+          <ChoosePdfButton
+            onPdfStoreSuccess={handlePdfFileStoreSuccess}
+            size="lg"
+            colorScheme="brand"
+            marginY="2rem"
+          >
+            {t("home.banner.choosePdfButtonText")}
+          </ChoosePdfButton>
+        </Flex>
+      </Container>
+    </Box>
   );
 };
