1957ebc50a702f12c49fdbbbfd52b778c75a680d
Nacht <nacht42@riseup.net>
2021-11-20 17:09:56 +0800

    feat(home banner): add some decoration

--- src/pages/home/components/HomeBanner.tsx
+++ src/pages/home/components/HomeBanner.tsx
@@ -11,11 +11,47 @@ import { useHistory } from "react-router";
 import routes from "../../../constants/routes.json";
 import { ChoosePdfButton } from "../../../common/components/ChoosePdfButton";
 import { useTranslation } from "react-i18next";
+import { MotionText } from "../../../common/components/motionComponents";
+import { useColor } from "../../../common/hooks/useColor";
+
+const DecorationText: React.FC = () => {
+  const colors = useColor();
+  const colorValue = useColorModeValue(
+    [
+      `linear-gradient(to left, ${colors.brand[500]}, ${colors.secondary[500]})`,
+      `linear-gradient(to left, ${colors.secondary[500]}, ${colors.brand[500]})`,
+    ],
+    [
+      `linear-gradient(to left, ${colors.brand[200]}, ${colors.secondary[200]})`,
+      `linear-gradient(to left, ${colors.secondary[200]}, ${colors.brand[200]})`,
+    ]
+  );
+  return (
+    <MotionText
+      fontSize="3xl"
+      margin="0"
+      animate={{
+        background: colorValue,
+        backgroundClip: "text",
+        WebkitBackgroundClip: "text",
+        WebkitTextFillColor: "transparent",
+      }}
+      transition={{
+        duration: 1,
+        repeat: Infinity,
+        repeatType: "mirror",
+      }}
+    >
+      ◭ ◭ ◭ ◭ ◭ ◭ ◭ ◭
+    </MotionText>
+  );
+};
 
 export const HomeBanner: React.FC = () => {
   const { t } = useTranslation();
-  const backgroundColorValue = useColorModeValue("brand.50", "grey.500");
+  const backgroundColorValue = useColorModeValue("brand.50", "gray.700");
   const history = useHistory();
+
   const handlePdfFileStoreSuccess = () => {
     history.replace(routes.OVERVIEW);
   };
@@ -29,6 +65,7 @@ export const HomeBanner: React.FC = () => {
           <Text fontSize="xl" textAlign="center" marginY="1rem">
             {t("home.banner.description")}
           </Text>
+          <DecorationText />
           <ChoosePdfButton
             onPdfStoreSuccess={handlePdfFileStoreSuccess}
             size="lg"
