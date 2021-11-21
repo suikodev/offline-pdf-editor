5de107ce1dbcd40ecd45441fb336ef85e6f45388
Nacht <nacht42@riseup.net>
2021-11-21 17:02:48 +0800

    feat(pdfcover): scroll long filename when hover on; add outline and boxShadow for pdf cover

--- src/pages/store/components/PdfCover.tsx
+++ src/pages/store/components/PdfCover.tsx
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { usePdfFileById } from "../../../common/hooks/usePdfFileById";
 import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
-import { Box, Flex, Skeleton, Text, Tooltip } from "@chakra-ui/react";
+import { Box, Flex, Skeleton, Tooltip } from "@chakra-ui/react";
 import { PDFDocument } from "pdf-lib";
 import { Pdf } from "../../../common/storage/PdfStore";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import { MotionText } from "../../../common/components/motionComponents";
+import { useColor } from "../../../common/hooks/useColor";
 
 const useCompatiblePageWidth = (
   pdf: Pdf | undefined,
@@ -26,19 +28,24 @@ const useCompatiblePageWidth = (
 const PdfCover: React.FC<PdfCoverProps> = (props) => {
   const { pdfId, width, height } = props;
   const [isLoaded, setIsLoaded] = React.useState(false);
+  const ref = useRef<HTMLParagraphElement>(null);
 
   const pdfFile = usePdfFileById(pdfId, []);
 
   const pageWidth = useCompatiblePageWidth(pdfFile, { width, height });
 
+  const colors = useColor();
+
   return (
-    <Box width={width} overflow="hidden">
+    <Box width={width}>
       <Skeleton
         as={Flex}
         height={height}
         isLoaded={isLoaded}
         alignItems="center"
         justifyContent="center"
+        outline={`1px ${colors.brand[300]} solid`}
+        boxShadow={` 6px 6px ${colors.brand[200]} `}
       >
         {pdfFile && (
           <Document
@@ -60,11 +67,35 @@ const PdfCover: React.FC<PdfCoverProps> = (props) => {
           </Document>
         )}
       </Skeleton>
-      <Skeleton isLoaded={isLoaded} marginTop="8px">
+      <Skeleton
+        display="flex"
+        isLoaded={isLoaded}
+        marginTop="8px"
+        overflow="hidden"
+      >
         <Tooltip label={pdfFile?.filename}>
-          <Text whiteSpace="nowrap" fontWeight="bold">
+          <MotionText
+            ref={ref}
+            whileHover={
+              (ref.current?.clientWidth || 0) > width
+                ? {
+                    x: [0, -((ref.current?.clientWidth || 0) - width)],
+                    transition: {
+                      duration: ((ref.current?.clientWidth || 0) - width) / 50,
+                      repeatDelay: 0.5,
+                      velocity: 0.1,
+                      type: "spring",
+                      repeat: Infinity,
+                      repeatType: "reverse",
+                    },
+                  }
+                : undefined
+            }
+            whiteSpace="nowrap"
+            fontWeight="bold"
+          >
             {pdfFile?.filename}
-          </Text>
+          </MotionText>
         </Tooltip>
       </Skeleton>
     </Box>
