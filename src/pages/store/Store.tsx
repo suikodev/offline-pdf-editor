5656ce68c1c57d4dba3b70011158758745cd2630
Nacht <nacht42@riseup.net>
2021-11-21 18:32:11 +0800

    feat(store): add heading

--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -1,7 +1,12 @@
-import { Box, Container, Grid } from "@chakra-ui/react";
+import { Box, Container, Grid, useColorModeValue } from "@chakra-ui/react";
 import React from "react";
 import { shallowEqual } from "react-redux";
+import {
+  MotionFlex,
+  MotionHeading,
+} from "../../common/components/motionComponents";
 import { useAppSelector } from "../../common/hooks";
+import { useColor } from "../../common/hooks/useColor";
 import { useRedirectToHomeWhenNoPdf } from "../../common/hooks/useRedirectToHomeWhenNoPdf";
 import PdfCover from "./components/PdfCover";
 
@@ -11,9 +16,22 @@ export const Store: React.FC = () => {
     (state) => state.pdfList.data?.map((i) => i.id) || [],
     shallowEqual
   );
+  const colors = useColor();
+  const headingBackgroundColor = useColorModeValue(
+    colors.brand[50],
+    colors.gray[700]
+  );
+  const headingColor = useColorModeValue(colors.black, colors.secondary[50]);
   return (
-    <Box as="section" paddingY="16px">
-      <Container as="section" maxW="container.xl">
+    <Box as="section">
+      <MotionFlex
+        backgroundColor={headingBackgroundColor}
+        justifyContent="center"
+        padding="16px"
+      >
+        <MotionHeading color={headingColor}>PDF store</MotionHeading>
+      </MotionFlex>
+      <Container maxW="container.xl" paddingY="16px">
         <Grid
           width="100%"
           templateColumns="repeat(5, 1fr)"
